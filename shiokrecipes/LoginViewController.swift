5f7e6ba0ae19c8392655369ab9b9c69e98131e41
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-06-10 18:28:10 +0800

    Create LoginViewController

---
+++ shiokrecipes/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  shiokrecipes
+//
+//  Created by Clarence Chan on 10/6/20.
+//  Copyright © 2020 clarencechan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var lbWelcome: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
