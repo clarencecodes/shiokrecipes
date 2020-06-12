bbc4d54b6d9f192fa0926c44a9a189ea88292e9a
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-06-12 17:11:48 +0800

    Log out when settingsTabBarItem is tapped

--- shiokrecipes/Controllers/TabBarController.swift
+++ shiokrecipes/Controllers/TabBarController.swift
@@ -9,23 +9,37 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    let exploreTabBarItem: UITabBarItem = {
+        let item = UITabBarItem()
+        item.title = "Explore"
+        item.image = UIImage(systemName: "magnifyingglass")
+        return item
+    }()
+    
+    let myRecipesTabBarItem: UITabBarItem = {
+        let item = UITabBarItem()
+        item.title = "My Recipes"
+        item.image = UIImage(systemName: "doc.text")
+        item.selectedImage = UIImage(systemName: "doc.text.fill")
+        return item
+    }()
+    
+    let favoritesTabBarItem: UITabBarItem = {
+        let item = UITabBarItem()
+        item.title = "Favorites"
+        item.image = UIImage(systemName: "heart")
+        item.selectedImage = UIImage(systemName: "heart.fill")
+        return item
+    }()
+    
+    let settingsTabBarItem: UITabBarItem = {
+        let item = UITabBarItem()
+        item.title = "Settings"
+        item.image = UIImage(systemName: "gear")
+        return item
+    }()
+    
     override func viewDidLoad() {
-        let exploreTabBarItem = UITabBarItem()
-        exploreTabBarItem.title = "Explore"
-        exploreTabBarItem.image = UIImage(systemName: "magnifyingglass")
-        
-        let myRecipesTabBarItem = UITabBarItem()
-        myRecipesTabBarItem.title = "My Recipes"
-        myRecipesTabBarItem.image = UIImage(systemName: "doc.plaintext")
-        
-        let favoritesTabBarItem = UITabBarItem()
-        favoritesTabBarItem.title = "Favorites"
-        favoritesTabBarItem.image = UIImage(systemName: "heart")
-        
-        let settingsTabBarItem = UITabBarItem()
-        settingsTabBarItem.title = "Settings"
-        settingsTabBarItem.image = UIImage(systemName: "gear")
-        
         let exploreVc = ExploreViewController()
         exploreVc.tabBarItem = exploreTabBarItem
         
@@ -35,12 +49,22 @@ class TabBarController: UITabBarController {
         let favoritesVc = FavoritesViewController()
         favoritesVc.tabBarItem = favoritesTabBarItem
         
-        // TODO: replace this with the correct ViewController
         let settingsVc = UIViewController()
         settingsVc.tabBarItem = settingsTabBarItem
         
         self.viewControllers = [exploreVc, myRecipesVc, favoritesVc, settingsVc]
     }
     
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        if item == settingsTabBarItem {
+            // TODO: present the SettingsViewController instead.
+            let loginVc = LoginViewController(nibName: "LoginViewController", bundle: nil)
+            let navigationController = UINavigationController(rootViewController: loginVc)
+            navigationController.isNavigationBarHidden = true
+            navigationController.isToolbarHidden = true
+            UIApplication.shared.windows.first?.rootViewController = navigationController
+            UIApplication.shared.windows.first?.makeKeyAndVisible()
+        }
+    }
     
 }
