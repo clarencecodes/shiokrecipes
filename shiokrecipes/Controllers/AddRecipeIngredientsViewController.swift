0e9f53ce9b095e5f4f882ec7cf083f1d064a83bd
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-07-09 15:39:21 +0800

    Create IngredientTextFieldCell

--- shiokrecipes/Controllers/AddRecipeIngredientsViewController.swift
+++ shiokrecipes/Controllers/AddRecipeIngredientsViewController.swift
@@ -30,22 +30,34 @@ class AddRecipeIngredientsViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.register(IngredientTextFieldCell.self, forCellReuseIdentifier: "IngredientTextFieldCell")
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+    
     // MARK: - View life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension AddRecipeIngredientsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientTextFieldCell") as! IngredientTextFieldCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
 }
---
+++ shiokrecipes/Views/IngredientTextFieldCell.swift
@@ -0,0 +1,58 @@
+//
+//  IngredientTextFieldCell.swift
+//  shiokrecipes
+//
+//  Created by Clarence Chan on 9/7/20.
+//  Copyright © 2020 clarencechan. All rights reserved.
+//
+
+import UIKit
+
+class IngredientTextFieldCell: UITableViewCell {
+
+    // MARK: - Views
+    
+    lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.cornerRadius = 5
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 2))
+        textField.leftViewMode = .always
+        textField.font = Constants.Design.Font.newYorkRegular.withSize(18)
+        return textField
+    }()
+    
+    // MARK: - Initialization code
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+        setupConstraints()
+    }
+    
+    // MARK: - View setup
+
+    private func setupViews() {
+        self.selectionStyle = .none
+        self.backgroundColor = .clear
+        contentView.addSubview(textField)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            textField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            textField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            textField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
+}
