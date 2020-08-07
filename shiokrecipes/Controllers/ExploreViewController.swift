f0463d49113ca47b35a3f1963c2616d056077020
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-08-07 16:26:35 +0800

    refactor

--- shiokrecipes/Controllers/ExploreViewController.swift
+++ shiokrecipes/Controllers/ExploreViewController.swift
@@ -16,5 +16,43 @@ class ExploreViewController: RecipeViewController {
     override func viewDidLoad() {
         self.titleLabel.text = "Explore"
         self.view.backgroundColor = Constants.Design.Color.orange
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
     }
+}
+
+
+// MARK: - CollectionViewDelegates & DataSource
+
+extension RecipeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! RecipeCollectionViewCell
+        
+        cell.authorNameLabel.text = "Jamie Oliver"
+        cell.titleLabel.text = "Pancakes with strawberries and blueberries"
+        
+        DispatchQueue.main.async {
+            if let url = URL(string: "https://images.unsplash.com/photo-1528207776546-365bb710ee93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&dpr=2&auto=format&fit=crop&w=140&h=200&q=60"),
+                let data = try? Data(contentsOf: url),
+                let image = UIImage(data: data) {
+                cell.dishImageView.image = image
+            }
+        }
+        
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (UIScreen.main.bounds.width / 2) - 28
+        let height = 1.65 * width
+        return CGSize(width: width, height: height)
+    }
+    
 }
--- shiokrecipes/Controllers/RecipeViewController.swift
+++ shiokrecipes/Controllers/RecipeViewController.swift
@@ -20,8 +20,6 @@ class RecipeViewController: UIViewController {
     }
     @IBOutlet weak var collectionView: UICollectionView! {
         didSet {
-            collectionView.delegate = self
-            collectionView.dataSource = self
             collectionView.contentInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
             
             collectionView.register(RecipeCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
@@ -35,38 +33,4 @@ class RecipeViewController: UIViewController {
     }
 
 
-}
-
-
-// MARK: - CollectionViewDelegates & DataSource
-
-extension RecipeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! RecipeCollectionViewCell
-        
-        cell.authorNameLabel.text = "Jamie Oliver"
-        cell.titleLabel.text = "Pancakes with strawberries and blueberries"
-        
-        DispatchQueue.main.async {
-            if let url = URL(string: "https://images.unsplash.com/photo-1528207776546-365bb710ee93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&dpr=2&auto=format&fit=crop&w=140&h=200&q=60"),
-                let data = try? Data(contentsOf: url),
-                let image = UIImage(data: data) {
-                cell.dishImageView.image = image
-            }
-        }
-        
-        return cell
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = (UIScreen.main.bounds.width / 2) - 28
-        let height = 1.65 * width
-        return CGSize(width: width, height: height)
-    }
-    
 }
